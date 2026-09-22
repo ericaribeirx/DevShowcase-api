5f69eda695cade4299370c9d68be582df1e277d1
ericaribeirx <ericaribeirx@gmail.com>
2026-09-22 18:23:03 -0300

    Ajusta porta da API para deploy

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
